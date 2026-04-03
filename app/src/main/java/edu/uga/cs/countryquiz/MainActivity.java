062c2eabc1ad1a9e9e951c27b2ecf08afeed0a86
Emily <yunseoechoi@gmail.com>
2026-04-03 11:41:36 -0400

    MainActivity Page Implemented

--- app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/countryquiz/MainActivity.java
@@ -1,81 +1,51 @@
 package edu.uga.cs.countryquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import android.content.res.Resources;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-import com.opencsv.CSVReader;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 
+/**
+ * Displays the app purpose, rules, and buttons.
+ * It initalizes the databases on the first app launch.
+ */
 public class MainActivity extends AppCompatActivity {
 
-    final String TAG = "CSVReading";
+    final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button rankingButton = findViewById( R.id.button );
-        rankingButton.setOnClickListener(new ButtonClickListener());
+        Log.d(TAG, "MainActivity Created");
+
+        initalizeDatabase();
+
+        Button startQuizButton = findViewById(R.id.button);
+        Button viewResultsButton = findViewById(R.id.button2);
+
+        startQuizButton.setOnClickListener(v -> startQuiz());
+        viewResultsButton.setOnClickListener(v -> viewResults());
     }
 
-    private class ButtonClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            try {
-                // Open the CSV data file in the assets folder
-                InputStream in_s = getAssets().open( "data.csv" );
+    private void initalizeDatabase() {
+        Log.d(TAG, "Initializing database");
 
-                // get the TableLayout view
-                TableLayout tableLayout = findViewById(R.id.table_main);
+        QuizDBHelper dbHelper = QuizDBHelper.getInstance(this);
+    }
 
-                // set up margins for each TextView in the table layout
-                android.widget.TableRow.LayoutParams layoutParams =
-                        new TableRow.LayoutParams( TableRow.LayoutParams.WRAP_CONTENT,
-                                TableRow.LayoutParams.WRAP_CONTENT );
-                layoutParams.setMargins(20, 0, 20, 0);
+    private void startQuiz() {
+        Log.d(TAG, "Start button clicked");
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
 
-                // read the CSV data
-                CSVReader reader = new CSVReader( new InputStreamReader( in_s ) );
-                String[] nextRow;
-                while( ( nextRow = reader.readNext() ) != null ) {
-
-                    // nextRow[] is an array of values from the line
-
-                    // create the next table row for the layout
-                    TableRow tableRow = new TableRow( getBaseContext() );
-                    for( int i = 0; i < nextRow.length; i++ ) {
-
-                        // create a new TextView and set its text
-                        TextView textView = new TextView( getBaseContext() );
-                        // for all columns exept the SCHOOL, align right
-                        if( i != 1 )
-                            textView.setGravity( Gravity.RIGHT );
-                        textView.setText( nextRow[i] );
-
-                        // add the new TextView to the table row in the table supplying the
-                        // layout parameters
-                        tableRow.addView( textView, layoutParams );
-                    }
-
-                    // add the next row to the table layout
-                    tableLayout.addView( tableRow );
-                }
-            } catch (Exception e) {
-                Log.e( TAG, e.toString() );
-            }
-        }
+    private void viewResults() {
+        Log.d(TAG, "View results button clicked");
+        Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+        startActivity(intent);
     }
 }
